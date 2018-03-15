f650e65bb87e11e8eebcfdecb8867507ab7f91b2
Derrick Carr <whyderrick@gmail.com>
2018-03-14 21:56:43 -0700

    Add class on click

--- src/listeners.js
+++ src/listeners.js
@@ -1,12 +1,19 @@
 import { getPhotos } from "./flickr.js";
 
-const attachThumbnails = data => {
+const loadThumbnails = (data = []) => {
   document.querySelectorAll(".thumbnail").forEach((el) => {
-    el.querySelector("img").setAttribute("src", "https:\/\/farm5.staticflickr.com\/4772\/38982783700_0e66a78cd1_s.jpg");
+    const index = el.getAttribute("data-thumbnail");
+    el.querySelector("img").setAttribute(
+      "src",
+      data[index] || "https:\/\/farm5.staticflickr.com\/4772\/38982783700_0e66a78cd1_s.jpg"
+    );
+    el.addEventListener("click", () => {
+      el.classList.add("lightbox");
+    });
   });
 };
 
 document.addEventListener("DOMContentLoaded", async function() {
   //const photoData = await getPhotos();
-  attachThumbnails();
+  loadThumbnails();
 });
