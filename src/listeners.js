2f5162461385957ca02b792fbf42b25305f15d61
Derrick Carr <whyderrick@gmail.com>
2018-03-14 21:37:45 -0700

    Add listener to attach thumbnails on load

--- src/listeners.js
+++ src/listeners.js
@@ -1,5 +1,12 @@
 import { getPhotos } from "./flickr.js";
 
+const attachThumbnails = data => {
+  document.querySelectorAll(".thumbnail").forEach((el) => {
+    el.querySelector("img").setAttribute("src", "https:\/\/farm5.staticflickr.com\/4772\/38982783700_0e66a78cd1_s.jpg");
+  });
+};
+
 document.addEventListener("DOMContentLoaded", async function() {
-  const photoData = await getPhotos();
+  //const photoData = await getPhotos();
+  attachThumbnails();
 });
